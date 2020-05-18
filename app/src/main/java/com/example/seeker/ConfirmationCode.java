078fab3d9304bea2fcf0eaab7e5028e652332d32
sanjanadz <h.sanjana1996@gmail.com>
2020-05-18 16:31:30 +0530

    Created Confirm Phone No UI

---
+++ app/src/main/java/com/example/seeker/ConfirmationCode.java
@@ -0,0 +1,14 @@
+package com.example.seeker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmationCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation_code);
+    }
+}

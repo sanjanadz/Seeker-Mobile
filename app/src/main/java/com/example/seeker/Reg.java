09c6d37e3e7e0ddb64d63756d76a495cf19b726e
sanjanadz <h.sanjana1996@gmail.com>
2020-05-19 13:41:10 +0530

    Complete Reg Code

--- app/src/main/java/com/example/seeker/Reg.java
+++ app/src/main/java/com/example/seeker/Reg.java
@@ -2,13 +2,65 @@ package com.example.seeker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class Reg extends AppCompatActivity {
+
+    private Spinner spinner;
+    private EditText editText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
-    }
+
+        spinner = findViewById(R.id.spinnerCountries);
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,CountryData.countryName));
+        editText = findViewById(R.id.textphonenumber);
+
+        findViewById(R.id.buttonsubmit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String code = CountryData.countryAreaCode[spinner.getSelectedItemPosition()];
+                String number = editText.getText().toString().trim();
+
+                if(number.isEmpty()){
+                    editText.setError("Number is required");
+                    editText.requestFocus();
+                    return;
+                }
+
+                if(number.length()<9 || number.length()>9){
+                    editText.setError("Enter Correct Number");
+                    editText.requestFocus();
+                    return;
+                }
+                String phoneNumber = "+" +code+number;
+
+
+                Intent intent = new Intent(Reg.this, ConfirmationCode.class);
+                intent.putExtra("phonenumber", phoneNumber);
+                startActivity(intent);
+
+            }
+        });
+
+
+        }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        if(FirebaseAuth.getInstance().getCurrentUser() !=null){
+//            Intent intent = new Intent( MainActivity.this, Profile.class);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//            startActivity(intent);        }
+        }
 }
